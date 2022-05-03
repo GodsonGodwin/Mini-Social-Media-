2c730624f5ebc35ba2c055f7101a12b8b224bb3b
unknown <godsongodwinsaben1998@gmail.com>
2022-05-03 01:15:13 +0100

    bug fixes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import LeftBar from './components/NavBar/LeftBar';
 import NavBar from './components/NavBar/NavBar';
 import { styled, alpha } from '@mui/material/styles';
 import RightBar from './components/NavBar/RightBar';
+import AddIcon from './components/NavBar/Add';
 
 
 
@@ -22,12 +23,15 @@ function App() {
     <>
       <NavBar />
       <Grid container>
-        <Grid item sm={2} xs={2}> 
+        <Grid item sm={3} xs={3}> 
         <LeftBar />
         </Grid>
-        <Grid item sm={7} xs={10}> <Feed /></Grid>
+        <Grid item sm={6} xs={9}> <Feed /></Grid>
         <XSGrid item sm={3} > <RightBar/></XSGrid>
       </Grid>
+
+      <AddIcon/>
+
     
   </>
   );
